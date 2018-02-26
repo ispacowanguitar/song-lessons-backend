290511df8ad2194fd523f3f2858fe239ef86591f
Alex Ispa-Cowan <alex.ispacowan@procore.com>
2018-02-25 18:34:52 -0800

    Display a list of songs on frontend

--- song-lessons-frontend/src/App.js
+++ song-lessons-frontend/src/App.js
@@ -1,11 +1,29 @@
 import React, { Component } from 'react';
 
 class App extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      songs: [""]
+    }
+  }
+  componentDidMount() {
+    this.setState({
+      songs: [
+        "All the things",
+        "Blue Bossa"
+      ]
+    })
+  }
   render() {
     return (
       <div>
-
-      </div>
+        <ul>
+          {this.state.songs.map(song => {
+            return <li key={song}>{song}</li>
+          })}
+        </ul>
+       </div>
     );
   }
 }
