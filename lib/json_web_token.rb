b5be2f1e7e30a9da51f5cb8e07a93b04735b7c51
Alex Ispa-Cowan <alex.ispacowan@procore.com>
2018-03-13 14:00:48 -0500

    Fix bug in token generation, fix typo in ActiveSupport::Concern

--- lib/json_web_token.rb
+++ lib/json_web_token.rb
@@ -1,11 +1,11 @@
 class JsonWebToken
-  def self.decode(payload, exp = 2.hours.from_now)
+  def self.encode(payload, exp = 2.hours.from_now)
     payload[:exp] = exp.to_i
-    JWT.decode(payload, Rails.application.secrets.secret_key_base)
+    JWT.encode(payload, Rails.application.secrets.secret_key_base)
   end
 
-  def self.encode(token)
-    body = JWT.encode(token, Rails.application.secrets.secret_key_base)[0]
+  def self.decode(token)
+    body = JWT.decode(token, Rails.application.secrets.secret_key_base)[0]
     HashWithIndifferentAccess.new body
     rescue JWT::ExpiredSignature, JWT::VerificationError => e
       raise ExceptionHandler::ExpiredSignature, e.message
