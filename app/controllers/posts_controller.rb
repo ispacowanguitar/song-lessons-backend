f8c23c32544f2b707b55f2c65b4da648532f20a8
Alex <Alex@Macbooks-MacBook-Pro-10.local>
2018-03-18 21:28:35 -0700

    remove unused stuff

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -9,14 +9,12 @@ class PostsController < ApplicationController
   def create
     post = Post.new(post_params)
     if post.save
-      render json: {message: "post saved successfully"}
+      render json: { message: "post saved successfully" }
     end
   end
 
   private
-  def index_params
-    params.permit(:song_id)
-  end
+
   def post_params
     params.permit(:user_id, :song_id, :url, :description)
   end
